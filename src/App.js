e17edd21939b0810215be412f917512dcd2208c2
musulmankul <sultanovmusulmankul@gmail.com>
2024-12-05 12:39:15 +0600

    button-size

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import Button from "./components/Button";
 function App() {
   return (
     <div>
+      <Button size ="sm">Confirm1</Button>
       <Button variant="bordered">Confirm</Button>
-      <Button>Cancel</Button>
+      <Button size="lg">Cancel</Button>
     </div>
 
   );
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 import "./index.css";
 function Button(props) {
-    const {variant = "primary", children} = props;
-    const className = `my-button my-button--${variant}`;
+    const {size = "md", variant = "primary", children} = props;
+    const className = `my-button my-button--${variant} my-button--${size}`;
 
     return <button className={className}>{children}</button>
 }
